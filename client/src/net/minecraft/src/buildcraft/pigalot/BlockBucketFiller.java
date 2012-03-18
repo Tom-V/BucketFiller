3bf3fc9bd91289af6b90497560b0070d65783702
Tom Vervoort <tv.vervoort@gmail.com>
2012-03-18 21:17:25 +0100

    Sneaking or holding a pipe doesnt show UI

--- client/src/net/minecraft/src/buildcraft/pigalot/BlockBucketFiller.java
+++ client/src/net/minecraft/src/buildcraft/pigalot/BlockBucketFiller.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import net.minecraft.src.*;
 import net.minecraft.src.buildcraft.core.Utils;
+import net.minecraft.src.buildcraft.transport.ItemPipe;
 import net.minecraft.src.forge.ITextureProvider;
 
 /**
@@ -36,7 +37,16 @@ public class BlockBucketFiller extends BlockContainer implements ITextureProvide
     @Override
     public boolean blockActivated(World world, int i, int j, int k,
 			EntityPlayer entityplayer){
-        switch(world.getBlockMetadata(i,j,k)) {
+    	
+    	// Drop through if the player is sneaking 
+    	if(entityplayer.isSneaking()) return false;
+    	
+    	// Do not open Gui's when having a pipe in hand 
+    	if (entityplayer.getCurrentEquippedItem() != null) 
+    		if( entityplayer.getCurrentEquippedItem().getItem() instanceof ItemPipe)
+    			return false; 	
+    	
+    	switch(world.getBlockMetadata(i,j,k)) {
             case 0:
                 TileBucketFiller tile = (TileBucketFiller) world.getBlockTileEntity(i, j, k);
                 PigalotProxie.displayGUIBucketFiller(entityplayer, tile);
